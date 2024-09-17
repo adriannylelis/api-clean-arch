c9a44cec67b78a710e45e5444acf605c5d0a14a3
adriannylelis <adrianny.lelis@gmail.com>
2024-09-16 22:53:42 -0400

    :wrench: chore: Env config modele

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,18 @@
-import { Module } from '@nestjs/common'
+import { DynamicModule, Module } from '@nestjs/common'
 import { EnvConfigService } from './env-config.service'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
-@Module({
+@Module(metadata:{
   providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule extends ConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return super.forRoot(options:{
+      ...options,
+      envFilePath: [
+        join(...paths:__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+      ],
+    })
+  }
+}
