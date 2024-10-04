406af784ac80114d1d588a1ded75e39bb86cd9ce
adriannylelis <adrianny.lelis@gmail.com>
2024-10-03 22:17:45 -0400

    :test_tube: test: Integration tests - validate createdAt field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -81,5 +81,19 @@ describe('User entity integration tests', () => {
       }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
+
+    it('Should throw an error when creating a user whit invalid createdAt', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        createdAt: '2023' as any,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+    })
   })
 })
