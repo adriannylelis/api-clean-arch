bd990d504de7a45da70b733cf0e3c0940aa2e52d
adriannylelis <adrianny.lelis@gmail.com>
2024-10-07 22:11:30 -0400

    :test_tube: test: InMemoryRepository unit tests - insert method

---
+++ src/shared/domain/repositories/__tests__/unit/in-mmory.repository.spec.ts
@@ -0,0 +1,27 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemoryRepository } from '../../in-mmory.repository'
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemoryRepository extends InMemoryRepository<StubEntity> {}
+
+describe('InMemoryRepository', () => {
+  let sut: StubInMemoryRepository
+
+  beforeEach(() => {
+    sut = new StubInMemoryRepository()
+  })
+
+  it('Should inserts a new entity', async () => {
+    const entity = new StubEntity({ name: 'any_name', price: 10 })
+
+    await sut.insert(entity)
+
+    expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+})
